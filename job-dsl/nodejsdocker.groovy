239641be18ed6e4cc7a4747561fee099a0a58a82
Jorge Alberto de Oliveira <jalberto@uolinc.com>
2020-10-19 17:19:32 -0300

    modificacao n7

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -13,26 +13,15 @@ job('NodeJS Docker example') {
         nodejs('nodejs') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
-    stages {
-        stage ('Build Image') {
-            steps {
-                dockerBuildAndPublish {
-                    repositoryName('seraph777/docker-nodejs-demo')
-                    tag('${GIT_REVISION,length=9}')
-                    registryCredentials('dockerhub')
-                    forcePull(false)
-                    forceTag(false)
-                    createFingerprints(false)
-                    skipDecorate()
-                }
-            }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('seraph777/docker-nodejs-demo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
         }
-        stage ('Mundo') {
-            steps {
-              sh 'echo "This is halloween !"'
-            }
-        }
-
-
     }
 }
