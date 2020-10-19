91a1c153dca6f710bd4ff09fea42c3441d49e7f0
Jorge Alberto de Oliveira <jalberto@uolinc.com>
2020-10-19 15:30:44 -0300

    alterações n3

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,9 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/seraph777/docker-demo.git') 
+        {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('seraph777')
+            node / gitConfigEmail('oliveira.jorgelalberto@gmail.com')
         }
     }
     triggers {
@@ -14,7 +15,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('seraph777/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
